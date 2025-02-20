5c8927987a4deca6af8480db430091c3301fdd1f
Rubén <161054880+Foompe@users.noreply.github.com>
2025-02-20 11:46:15 +0100

    Creación de excepciones

---
+++ src/main/java/com/prog072_Excepciones/FaltaRecursos.java
@@ -0,0 +1,7 @@
+package com.prog072_Excepciones;
+
+public class FaltaRecursos extends Exception{
+
+
+
+}
---
+++ src/main/java/com/prog072_Excepciones/VelocidadMaximaException.java
@@ -0,0 +1,5 @@
+package com.prog072_Excepciones;
+
+public class VelocidadMaximaException extends Exception{
+    
+}
